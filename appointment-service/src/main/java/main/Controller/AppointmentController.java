d06d56b9d48653c7c9488b9e1ffc4dad9de382cf
bala <bala43957@gmail.com>
2026-06-24 19:51:08 +0530

    24th commit

--- appointment-service/src/main/java/main/Controller/AppointmentController.java
+++ appointment-service/src/main/java/main/Controller/AppointmentController.java
@@ -1,5 +1,34 @@
 package main.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import main.Dto.AppointmentDto;
+import main.Service.AppointmentService;
+
+@RestController
+@RequestMapping("/appointment")
 public class AppointmentController {
+	
+	
+	@Autowired
+	private AppointmentService service;
+	
+
+//booking
+	
+	@PostMapping("/booking")
+	public String booking(@Valid @RequestBody AppointmentDto dto) {
+		
+		
+		return service.booking(dto);
+	}
+	
+
+
 
 }
