1fd41bd1b3cfd8ad7f213cc4b915c41d5f0b06e7
bala <bala43957@gmail.com>
2026-06-24 11:40:00 +0530

    9th commit addedd repository

---
+++ patient-service/src/main/java/main/Repository/PatientRepository.java
@@ -0,0 +1,13 @@
+package main.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import main.Entity.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient,Integer>{
+	
+	Optional<Patient>findById(Integer id);
+
+}
